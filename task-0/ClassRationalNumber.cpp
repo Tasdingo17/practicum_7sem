4f6a090830d2d62682fe949daeb9548326b5a2dd
dmitriy <khasanovdima17@yandex.ru>
2022-09-15 20:30:45 +0300

    Modify Rational_number: numbers are stored backwards

--- task-0/ClassRationalNumber.cpp
+++ task-0/ClassRationalNumber.cpp
@@ -3,16 +3,16 @@
 #include "ClassRationalNumber.h"
 
 
-void Rational_number::move_minus_to_numerator(){
-    if (denominator[0] != '-'){
-        throw 1;  // TODO: make special exception
-    }
-    denominator.erase(0, 1);  //erase '-'
-    if (numerator[0] == '-'){
-        numerator.erase(0, 1);
-    } else if (numerator != "0") {
-        numerator.insert(0, 1, '-');
-    }
+int max(int a, int b){
+    return (a >= b) ? a : b;
+}
+
+// erase a '-' sign and return int value as reversed string
+std::string int_to_val_reversed(int x){         
+    int a = (x >= 0) ? x : -x;
+    std::string s = std::to_string(a);
+    std::reverse(s.begin(), s.end());
+    return s;
 }
 
 
@@ -27,41 +27,56 @@ bool Rational_number::is_number(const std::string& s) const{
 }
 
 
-Rational_number::Rational_number(): numerator("0"), denominator("1") {};
+Rational_number::Rational_number(): numerator("0"), denominator("1"),
+                                    is_negative(false) {};
 
 
 Rational_number::Rational_number(const std::string& m, const std::string& n):
     numerator(m), denominator(n) {
+    is_negative = ((m[0] == '-') != (n[0] == '-')) && (m != "0");  // xor
     if (!is_number(numerator) || !is_number(denominator)) {
         throw 2;  // TODO: make special exception
     }
     if (m == "0"){
         denominator = std::string("1");
+        is_negative = false;
         return;
     }
-    if (n[0] == '-') move_minus_to_numerator();
+    if (numerator[0] == '-') numerator.erase(0, 1);
+    if (denominator[0] == '-') denominator.erase(0, 1);
+    std::reverse(numerator.begin(), numerator.end());
+    std::reverse(denominator.begin(), denominator.end());
 };
 
 
-Rational_number::Rational_number(int m, int n): 
-    numerator(std::to_string(m)), denominator(std::to_string(n)) {
+Rational_number::Rational_number(int m, int n){
+    is_negative = (m >= 0) != (n > 0);  // xor
     if (n == 0){
         throw 2;  // TODO: make special exception
     }
+    numerator = int_to_val_reversed(m);
     if (m == 0){
         denominator = std::string("1");
+        is_negative = false;
         return;
     }
-    if (n < 0){  // Move a minus sign to numerator
-        move_minus_to_numerator();
-    }
+    denominator = int_to_val_reversed(n);
 };
 
 
 Rational_number::Rational_number(const Rational_number& other):
-    numerator(other.numerator), denominator(other.denominator) {};
+    numerator(other.numerator), denominator(other.denominator), 
+    is_negative(other.is_negative) {};
 
 
 std::ostream& operator<<(std::ostream &os, const Rational_number& n){
-    return os  << n.numerator << "/" << n.denominator;
+    std::string tmp1 = n.numerator, tmp2 = n.denominator;
+    std::reverse(tmp1.begin(), tmp1.end()); 
+    std::reverse(tmp2.begin(), tmp2.end());
+    return os << (n.is_negative ? "-" : "") << tmp1 << '/' << tmp2;
 }
+
+
+//bool operator==(const Rational_number& lhs, const Rational_number& rhs){
+//    return lhs.numerator * rhs.denominator == lhs.denominator * rhs.numerator;
+//}
--- task-0/ClassRationalNumber.h
+++ task-0/ClassRationalNumber.h
@@ -6,10 +6,11 @@
 
 class Rational_number{
 private:
-    std::string numerator;
-    std::string denominator;
+    std::string numerator;   // reversed
+    std::string denominator; // reversed
+    bool is_negative;
     bool is_number(const std::string& s) const;
-    void move_minus_to_numerator();
+    //void move_minus_to_numerator();
 public:
     Rational_number();
     Rational_number(const std::string& m, const std::string& n = "1");
@@ -18,6 +19,11 @@ public:
     const std::string& getNumerator() const;
     const std::string& getDenominator() const;
     friend std::ostream& operator<<(std::ostream &os, const Rational_number& n);
+    Rational_number operator+(const Rational_number& rhs) const;
+    friend Rational_number operator-(const Rational_number& lhs, const Rational_number& rhs);
+    friend Rational_number operator*(const Rational_number& lhs, const Rational_number& rhs);
+    friend Rational_number operator/(const Rational_number& lhs, const Rational_number& rhs);
+    friend bool operator==(const Rational_number& lhs, const Rational_number& rhs);
 };
 
 
