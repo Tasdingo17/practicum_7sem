74992a5164356aebab0f9532961d3a7d207b2cc5
tasdingo17 <khasanovdima17@yandex.ru>
2022-12-17 21:32:07 +0300

    Upgrade survaival func to use GoL

--- task-3/src/SurvaivalFunc.cpp
+++ task-3/src/SurvaivalFunc.cpp
@@ -3,6 +3,10 @@
 
 #include <memory>
 #include <bitset>
+#include "GoLEngine.hpp"
+
+#define iterN 100
+#define PENALTY 2500
 
 template <size_t N>
 class SurvaivalFunc{
@@ -16,13 +20,23 @@ public:
 template <size_t N>
 class GoLSurvaivalFunc : public SurvaivalFunc<N>{
 public:
+    GoLSurvaivalFunc(): engine(){}
+
     virtual std::unique_ptr<SurvaivalFunc<N>> clone() const override{
         return std::make_unique<GoLSurvaivalFunc<N>>(*this);
     }
 
     virtual int get_criterion_val(const std::bitset<N>& individ) override{
-        return individ.count();
+        int res;
+        auto target_desc = engine.get_great_descedant(individ, iterN);
+        res = target_desc.count();
+        if (target_desc == engine.get_descedant(target_desc)){
+            res += PENALTY;
+        }
+        return res;
     }
+private:
+    GoLEngine engine;
 };
 
 #endif  // __SurvFunc__
