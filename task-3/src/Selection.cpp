7fc9c3e41252faff9f9df7c9fecaf5a0831a7f75
tasdingo17 <khasanovdima17@yandex.ru>
2022-12-18 17:21:07 +0300

    Rename template argument

--- task-3/src/Selection.cpp
+++ task-3/src/Selection.cpp
@@ -9,22 +9,22 @@
 template<size_t N> 
 using array_indices = std::array<int, N>;
 
-template<size_t N>
+template<size_t popSize>
 class Selection{
 public:
     virtual ~Selection() {};
-    virtual std::unique_ptr<Selection<N>> clone() const = 0;
-    virtual array_indices<N> select(const std::array<int, N>& surv_func_vals) = 0;
+    virtual std::unique_ptr<Selection<popSize>> clone() const = 0;
+    virtual array_indices<popSize> select(const std::array<int, popSize>& surv_func_vals) = 0;
 };
 
 //#include <iostream>
 
 /*Roulette selection*/
-template <size_t N>
-class RouletteSelection : public Selection<N>{
+template <size_t popSize>
+class RouletteSelection : public Selection<popSize>{
 public:
-    virtual std::unique_ptr<Selection<N>> clone() const override{
-        return std::make_unique<RouletteSelection<N>>(*this);
+    virtual std::unique_ptr<Selection<popSize>> clone() const override{
+        return std::make_unique<RouletteSelection<popSize>>(*this);
     }
 
     /*1) Find probabilities
@@ -32,9 +32,9 @@ public:
     * 3) Go through probs and substract each element until P <= 0;
     * 4) Current index is the answer (or current index - 1, depending on implementation)
     */
-    virtual array_indices<N> select(const std::array<int, N>& surv_func_vals) override{
-        array_indices<N> res;
-        std::array<double, N> probs = std::move(vals_to_probs(surv_func_vals));
+    virtual array_indices<popSize> select(const std::array<int, popSize>& surv_func_vals) override{
+        array_indices<popSize> res;
+        std::array<double, popSize> probs = std::move(vals_to_probs(surv_func_vals));
         double tmp_start = 0.0;
         double sum_of_probs = std::accumulate(probs.begin(), probs.end(), tmp_start);
 
@@ -42,10 +42,10 @@ public:
         std::mt19937 rng(dev());
         std::uniform_real_distribution<> selection_prob_dist(0, sum_of_probs);
 
-        for (unsigned i = 0; i < N; i++){
+        for (unsigned i = 0; i < popSize; i++){
             double tmp_val = selection_prob_dist(rng);
             unsigned idx = 0;
-            for (idx = 0; idx < N; idx++){
+            for (idx = 0; idx < popSize; idx++){
                 tmp_val -= probs[idx];
                 if (tmp_val <= 0){
                     break;   
@@ -57,10 +57,10 @@ public:
         return res;
     }
 private:
-    std::array<double, N> vals_to_probs(const std::array<int, N>& surv_func_vals){
+    std::array<double, popSize> vals_to_probs(const std::array<int, popSize>& surv_func_vals){
         int total_sum = std::accumulate(surv_func_vals.begin(), surv_func_vals.end(), 0);
-        std::array<double, N> res;
-        for (unsigned i = 0; i < N; i++){
+        std::array<double, popSize> res;
+        for (unsigned i = 0; i < popSize; i++){
             res[i] = 1 - 1.0 * surv_func_vals[i] / total_sum ;     // the lower res the higher prob
         }
         return res;
